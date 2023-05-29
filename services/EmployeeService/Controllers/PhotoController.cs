015d00f8762a59a7b6e997805f5a4b083e73b6e5
jschec <scheck.joshua@gmail.com>
2023-05-28 22:30:10 -0700

    Streamlined routes in PhotoController.cs

--- services/EmployeeService/Controllers/PhotoController.cs
+++ services/EmployeeService/Controllers/PhotoController.cs
@@ -1,29 +1,64 @@
-using EmployeeService.Utils;
+using Amazon.DynamoDBv2.DataModel;
 using Microsoft.AspNetCore.Mvc;
+
+using EmployeeService.Models;
+using EmployeeService.Utils;
 
 namespace EmployeeService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("employees")]
 public class PhotoController : ControllerBase
 {
 
-    private readonly IS3Service _context;
+    private readonly IS3Service _storageContext;
+    private readonly IDynamoDBContext _dbContext;
     private readonly ILogger<PhotoController> _logger;
 
-    public PhotoController(IS3Service context, ILogger<PhotoController> logger)
+    public PhotoController(
+        IDynamoDBContext dbContext,
+        IS3Service storageContext, 
+        ILogger<PhotoController> logger)
     {
-        _context = context;
+        _dbContext = dbContext;
+        _storageContext = storageContext;
         _logger = logger;
     }
     
-    [HttpPost("upload")]
-    public async Task<IActionResult> Upload([FromForm] IFormFile file)
+    [HttpGet("{employeeId}/photo")]
+    public async Task<IActionResult> DownloadObject(string employeeId)
     {
-        var result = await _context.UploadFile(uploadFile.File);
-        return Ok(new
+        Employee record = await _dbContext
+            .LoadAsync<Employee>(employeeId);
+        
+        if (record == null) return NotFound();
+        
+        string objectUrl = await _storageContext.GetObjectUrl(
+            record.PhotoObjectBucket, record.PhotoObjectKey);
+        
+        return Ok(new { path = objectUrl });
+    }
+    
+    [HttpPost("{employeeId}/photo")]
+    public async Task<IActionResult> UploadObject(
+        string employeeId, [FromForm] IFormFile file)
+    {
+        var record = await _dbContext.LoadAsync<Employee>(employeeId);
+
+        if (record == null)
         {
-            path = result
-        });
+            return NotFound();
+        }
+        
+        string objectUri = await _storageContext.UploadFile(file);
+        // Remove S3 URI prefix and split by '/'
+        string[] parsedUri = objectUri[4..].Split('/');
+        
+        record.PhotoObjectBucket = parsedUri[0];
+        record.PhotoObjectKey = string.Join("/", parsedUri[1..]);
+
+        await _dbContext.SaveAsync(record);
+
+        return Ok(record);
     }
 }
