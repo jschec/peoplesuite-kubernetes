0164bb4d85e788aea8ce27f47f8dc6abc705fea3
jschec <scheck.joshua@gmail.com>
2023-05-29 20:45:37 -0700

    Updated entry point of DepartmentService

--- services/DepartmentService/Program.cs
+++ services/DepartmentService/Program.cs
@@ -1,11 +1,31 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Extensions.NETCore.Setup;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+////////////////////////////////////
+// Add services to the container. //
+////////////////////////////////////
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// AWS Services configuration
+AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
+builder.Services.AddDefaultAWSOptions(awsOptions);
+// Add DynamoDB
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
+
+////////////////////////////////////
 
 var app = builder.Build();
 
