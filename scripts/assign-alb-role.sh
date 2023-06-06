917d94cf4d5c3ceba21722059a845518cb706fb9
jschec <scheck.joshua@gmail.com>
2023-06-05 23:14:32 -0700

    Added assign-alb-role.sh script

---
+++ scripts/assign-alb-role.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# Load environmental variables
+source $SCRIPTS_DIR/load-env-data.sh
+
+# Create ALB Policy
+policy_arn=$(aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://infrastructure/alb-iam-policy.json \
+    | jq -r .Policy.Arn )
+
+# Create service account
+eksctl create iamserviceaccount \
+  --cluster=$CLUSTER_NAME \
+  --namespace=kube-system \
+  --name=aws-load-balancer-controller \
+  --role-name AmazonEKSLoadBalancerControllerRole \
+  --attach-policy-arn=$policy_arn \
+  --approve
