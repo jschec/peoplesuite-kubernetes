5d583db578852382ce9c6d07abfd09a5e87e40e9
jschec <scheck.joshua@gmail.com>
2023-06-05 23:22:45 -0700

    Added install-alb-controller.sh script

---
+++ scripts/install-alb-controller.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# Load environmental variables
+source $SCRIPTS_DIR/load-env-data.sh
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+  -n kube-system \
+  --set clusterName=$CLUSTER_NAME \
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=aws-load-balancer-controller 
